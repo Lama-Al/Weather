aadd36dfe989897664dcd0de7b0fea064a03bc26
Lama <84765301+Lama-Aldhafeeri@users.noreply.github.com>
2022-01-24 10:16:57 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 
-//
+
 let theCity = document.getElementById("theCity");
 let URL1 ='http://api.weatherstack.com/current?access_key=b150792e3adac9354fa611602845f03d&query=';
 let tempID=document.getElementById("temp");
@@ -7,7 +7,7 @@ let iconID=document.getElementById("icon");
 let descriptionID=document.getElementById("description");
    function setCity(){
 let chosenCity=theCity.value;
-//"Riyadh";
+
   URL1=URL1+chosenCity;
 //----------------------------------------
 const request = new XMLHttpRequest();
@@ -30,9 +30,7 @@ function josnParser(Josn){
     descriptionID.innerText=descrip;
     let Wicon=(myJosn.current.weather_icons).innerText;
     iconID.src="Wicon";
-    //let Wicons =parsed_json['current_observation']['icon'];
-    //myJosn.current.weather_icons;
-    //iconID.innerHTML=icons;
+    
     
     }
 
